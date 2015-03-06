1e4e2b186c257a060f2030f80ebd8f2a1ec03777
A-Circle-Zhang <cctvyay@163.com>
2015-03-07 00:12:30 +0800

    Update and rename server to server/getIqiyiVideo.php
    已经放弃使用

---
+++ server/getIqiyiVideo.php
@@ -0,0 +1,10 @@
+<?php
+require "core/base.class.php";
+require "core/iqiyi.class.php";
+$vid = $_GET['vid'];
+$tvid = $_GET['tvid'];
+$bid = intval($_GET['bid']);
+$part = intval($_GET['part']);
+Iqiyi::parse("http://www.iqiyi.com/vid_".$vid.":".$tvid.".html",$bid,$part);
+exit;
+?>
